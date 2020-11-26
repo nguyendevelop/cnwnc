a9e7397197db22e1e11011ac7715af00f2e0e0f1
Nguyễn Develop <63544549+nguyendevelop@users.noreply.github.com>
2020-11-26 09:02:37 +0700

    thuc hanh

--- demolara/routes/web.php
+++ demolara/routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -10,7 +12,32 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+// frontend
+Route::get('/','HomeController@index');
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/trang-chu','HomeController@index');
+// backend
+Route::get('/admin','AdminController@index');
+Route::get('/dashboard','AdminController@show_drashboard');
+Route::get('/logout','AdminController@logout');
+Route::post('/admin-dashboard','AdminController@dashboard');
+
+//Category Product
+Route::get('/add-category-product','CategoryProduct@add_category_product');
+Route::get('/all-category-product','CategoryProduct@all_category_product');
+Route::post('/save-category-product','CategoryProduct@save_category_product');
+//Route::post('/update-category-product/{category_product_id}','CategoryProduct@update_category_product');
+Route::get('/unactive-category-product/{category_product_id}','CategoryProduct@unactive_category_product');
+Route::get('/active-category-product/{category_product_id}','CategoryProduct@active_category_product');
+
+
+// thu nghiem
+Route::get('/product', function () {
+    return view('pages.product');
+});
+Route::get('/news', function () {
+    return view('pages.news');
+});
+Route::get('/contact', function () {
+    return view('pages.contact');
 });
