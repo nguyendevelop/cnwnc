4b7bcc75184ed9d9185cc72f8e04530c2ac880d9
Nguyễn Develop <63544549+nguyendevelop@users.noreply.github.com>
2020-12-16 11:18:30 +0700

    thuc_hanh

--- bai12/connect.php
+++
@@ -1,1 +0,0 @@
-$conn = mysqli_connect("localhost", "root", "", "rating") or die("Khong the ket noi toi CSDL");
---
+++ bai12/index.php
@@ -0,0 +1,24 @@
+<?php
+$conn = mysqli_connect("localhost", "root", "", "rating") or die("Khong the ket noi toi CSDL");
+?>
+<?php
+include("functions.php");
+
+if(isset($_GET['id'])){
+	$id=$_GET['id'];
+	// UPDATE RATING
+	if(isset($_POST['rate_submit'])){
+		if(isset($_POST['rate'])){
+			$rate = $_POST['rate'];
+			setRatingInfo($id, $rate);
+		}
+	}
+
+	$html = getProduct($id);
+	include("template/view-product.php");
+}else{
+	$html = listProduct();
+	include("template/list-products.php");
+}
+
+?>
